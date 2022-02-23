b642ccff0c598b994d78c6cc0523998f7af154f5
Plamen Dragiyski <plamen@dragiyski.org>
2022-02-23 02:33:03 +0100

    wip: display management

--- src/dragiyski/ui/_display.py
+++ src/dragiyski/ui/_display.py
@@ -1,26 +1,26 @@
 from sdl2 import *
 from ._error import UIError
-from ._event_thread import get_delegate_from_args
+from ._event_thread import delegate_sync_call
 from ._sdl import ensure_subsystem
 
-def count(**kwargs):
-    delegate_call = get_delegate_from_args(kwargs)
-    return delegate_call(_sdl_num_video_displays)
-
-def name(index: int, **kwargs):
-    delegate_call = get_delegate_from_args(kwargs)
-    return delegate_call(_sdl_display_name, index)
-
-def _sdl_num_video_displays():
-    ensure_subsystem(SDL_INIT_VIDEO)
-    return SDL_GetNumVideoDisplays()
-
-def _sdl_display_name(index):
-    ensure_subsystem(SDL_INIT_VIDEO)
+def count():
+    if not SDL_WasInit(SDL_INIT_VIDEO):
+        delegate_sync_call(SDL_InitSubSystem, SDL_VideoInit)
     count = SDL_GetNumVideoDisplays()
-    if count > 0 and index >= count:
-        raise IndexError(f'index must be in range [0, {count - 1}]')
-    name = SDL_GetDisplayName(index)
-    if name is None:
+    if count < 0:
         raise UIError
-    return name.decode('utf-8')
+
+def name(index: int):
+    if not SDL_WasInit(SDL_INIT_VIDEO):
+        delegate_sync_call(SDL_InitSubSystem, SDL_VideoInit)
+    count = SDL_GetNumVideoDisplays()
+    if count < 0:
+        raise UIError
+    elif count == 0:
+        raise IndexError(f'No displays found')
+    if index < 0 or index >= count:
+        raise IndexError(f'Index must be in range [0, {count - 1}]')
+    buffer = SDL_GetDisplayName(index)
+    if buffer is None:
+        raise UIError
+    return buffer.decode('utf-8')
