4a683cf30821d7faf64074eebfb6db194b93403e
Plamen Dragiyski <plamen@dragiyski.org>
2022-02-24 12:01:17 +0100

    src: module structure and bugfixes
    bugfix: wrong argument for SDL_InitSubsystem
    
    module structure: creating public `display` module to prevent `SDL_*`
    and other implementation-related modules from leaking into user's
    context.

--- src/dragiyski/ui/_display.py
+++ src/dragiyski/ui/_display.py
@@ -3,24 +3,24 @@ from ._error import UIError
 from ._event_thread import delegate_sync_call
 from ._sdl import ensure_subsystem
 
-def count():
-    if not SDL_WasInit(SDL_INIT_VIDEO):
-        delegate_sync_call(SDL_InitSubSystem, SDL_VideoInit)
-    count = SDL_GetNumVideoDisplays()
-    if count < 0:
-        raise UIError
-
-def name(index: int):
+def count() -> int:
     if not SDL_WasInit(SDL_INIT_VIDEO):
         delegate_sync_call(SDL_InitSubSystem, SDL_INIT_VIDEO)
     count = SDL_GetNumVideoDisplays()
     if count < 0:
         raise UIError
-    elif count == 0:
+    return count
+
+def name(index: int) -> str:
+    c = count()
+    if c < 0:
+        raise UIError
+    elif c == 0:
         raise IndexError(f'No displays found')
-    if index < 0 or index >= count:
+    if index < 0 or index >= c:
         raise IndexError(f'Index must be in range [0, {count - 1}]')
     buffer = SDL_GetDisplayName(index)
     if buffer is None:
         raise UIError
     return buffer.decode('utf-8')
+
